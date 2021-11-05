75e86921a4f32b6fae52fa91f8d8f0ae97dcbeac
Atharva Deolalikar <atharvadeolalikar@gmail.com>
2021-11-06 00:38:24 +0530

    type fixed

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FB_API_KEY,
-  authDomain: REACT_APP_FB_AUTH_DOMAIN,
-  projectId: REACT_APP_FB_PROJECT_ID,
-  storageBucket: REACT_APP_FB_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_FB_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FB_APP_ID
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID
 };
 
 export default firebaseConfig;

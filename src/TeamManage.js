27f92909fb5d9971eb43f7a4d5c8f679026c80b1
Atharva Deolalikar <atharvadeolalikar@gmail.com>
2021-11-13 22:57:02 +0530

    tooltip added

--- src/TeamManage.js
+++ src/TeamManage.js
@@ -23,8 +23,10 @@ function TeamManage(){
         <Box sx={{display: "flex", justifyContent: "space-evenly", flexWrap: "wrap", alignItems: "center", width : "100%", minHeight: "80vh", textAlign: "center" , my:7 , py:3}}>      
             <Box>
                 <Chip sx={{fontSize: {xs: 17, md: 22}, p:3.5 , my:2}} label={`Team: ${context.team.teamName}`}></Chip> 
-                <Alert sx={{borderRadius:3,  maxWidth: 400, my:2}} severity="info">This hackathon let’s you have upto 4 teammates. You can invite people to join your team by clicking the button below.</Alert>
-                <Button variant="outlined" onClick={() => setInviteDiag(true)}>Invite people</Button>
+                <Alert sx={{borderRadius:3,  maxWidth: 380, my:2}} severity="info">This hackathon let’s you have upto 4 teammates. You can invite people to join your team by clicking the button below.</Alert>
+                 
+                <Button size="large" fullWidth variant="outlined" onClick={() => setInviteDiag(true)}>Invite people</Button> 
+                
                 <Dialog open={inviteDiag}  fullWidth="xs">
                     <DialogTitle>Invite people to join your team</DialogTitle>
                     <DialogContent>    
@@ -32,38 +34,49 @@ function TeamManage(){
                             <CopyToClipboard
                                 options={{ debug: true, message: "" }}
                                 text={`https://hack4good.ieee-cis-sbc.org/team/join?teamID=${context.userData.teamID}`}
-                                onCopy={() => context.showAlert("success", "Invite link has been copied to Clipboard!")}>
-                                <TextField 
-                                    defaultValue={`https://hack4good.ieee-cis-sbc.org/team/join?teamID=${context.userData.teamID}`}
-                                    disabled
-                                    sx={{mt:1, mb:3}}
-                                    fullWidth
-                                    InputProps={{
-                                        endAdornment: <InputAdornment position="end"><IconButton ><LinkIcon /></IconButton></InputAdornment>
-                                    }}
-                                    
-                                ></TextField>
+                                onCopy={() => context.showAlert("success", "Team invite link has been copied to Clipboard!")}>
+                                <Tooltip title="Copy the team invite link and share it with your teammates!" arrow> 
+                                    <TextField 
+                                        defaultValue={`https://hack4good.ieee-cis-sbc.org/team/join?teamID=${context.userData.teamID}`}
+                                        disabled
+                                        sx={{mt:1, mb:3}}
+                                        fullWidth
+                                        InputProps={{
+                                            endAdornment: 
+                                            <InputAdornment position="end">
+                                                    <IconButton >
+                                                        <LinkIcon />
+                                                    </IconButton>
+                                            </InputAdornment>
+                                        }}
+                                        
+                                    ></TextField>
+                                </Tooltip>
                             </CopyToClipboard>
 
                         <Divider orientation="horizontal"><Chip label="Or"> </Chip></Divider>
 
                         <DialogContentText sx={{mt:2}} color="white">2. Share the team code </DialogContentText>
+                        
                             <CopyToClipboard
                                 options={{ debug: true, message: "" }}
                                 text={context.userData.teamID}
                                 onCopy={() => context.showAlert("success", "Team ID has been copied to Clipboard!")}>
-                                <TextField 
-                                    defaultValue={context.userData.teamID}
-                                    sx={{mt:1, mb:3}}
-                                    disabled
-                                    fullWidth
-                                    InputProps={{
-                                        endAdornment: <Tooltip title="Copy" arrow><InputAdornment position="end"><IconButton ><ContentCopyIcon /></IconButton></InputAdornment></Tooltip>
-                                    }}
-                            ></TextField>
+                                    <Tooltip title="Copy the team code and share it with your teammates" arrow> 
+                                        <TextField 
+                                            defaultValue={context.userData.teamID}
+                                            sx={{mt:1, mb:3}}
+                                            disabled
+                                            fullWidth
+                                            InputProps={{
+                                                endAdornment: <Tooltip title="Copy" arrow><InputAdornment position="end"><IconButton ><ContentCopyIcon /></IconButton></InputAdornment></Tooltip>
+                                            }}
+                                        ></TextField>
+                                    </Tooltip>
                             </CopyToClipboard>
+                       
                         <DialogActions >
-                            <Button variant="outlined"  onClick={() => setInviteDiag(false)}>
+                            <Button variant="outlined" onClick={() => setInviteDiag(false)}>
                                 Done
                             </Button> 
                         </DialogActions>
